7b48727f27b9413c733de2fd1ae0f05c34be2de9
MakerSquare Student <students@themakersquare.com>
2013-12-03 07:38:34 -0600

    Add code to identify and print students that are in the junior year of high school

--- student.rb
+++ student.rb
@@ -1,23 +1,32 @@
 class Student
 
-	attr_reader :first_name, :last_name, :grade
-	def initialize(first_name, last_name, grade)
-		@first_name = first_name
-		@last_name = last_name
-		@grade = grade
-	end
+  attr_reader :first_name, :last_name, :grade
 
-	def senior?
-		grade == 12
-	end
+  def initialize(first_name, last_name, grade)
+    @first_name = first_name
+    @last_name = last_name
+    @grade = grade
+  end
 
-	def to_s
-		"#{last_name}, #{first_name}"
-	end
+  def senior?
+    grade == 12
+  end
+
+  def junior?
+    grade == 11
+  end
+
+  def to_s
+    "#{last_name}, #{first_name}"
+  end
 end
 
 def seniors(students)
-	students.select { |student| student.senior? }
+  students.select { |student| student.senior? }
+end
+
+def juniors(students)
+  students.select { |student| student.junior? }
 end
 
 fred = Student.new("Fred", "James", 12)
@@ -25,6 +34,14 @@ sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
+puts "\nSeniors"
+
 seniors(all_students).each do |student|
-	puts student
+  puts " * #{student}"
+end
+
+puts "\nJuniors"
+
+juniors(all_students).each do |student|
+  puts " * #{student}"
 end
